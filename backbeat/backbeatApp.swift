cd4b74a8fe7608e95c806e6f4f21e5cbd528d2e1
awoniyosh <awoniyoshi00@gmail.com>
2026-03-01 00:10:13 +1100

    Initial Commit

---
+++ backbeat/backbeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  backbeatApp.swift
+//  backbeat
+//
+//  Created by tanakar on 1/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct backbeatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ backbeatTests/backbeatTests.swift
@@ -0,0 +1,17 @@
+//
+//  backbeatTests.swift
+//  backbeatTests
+//
+//  Created by tanakar on 1/3/2026.
+//
+
+import Testing
+@testable import backbeat
+
+struct backbeatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
